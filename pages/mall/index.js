0029f665f14172963ee5d8201fbf35c06fe0d2c6
fangkai <1209683835@qq.com>
2021-12-09 18:16:10 +0800

    feat: 商城

--- pages/mall/index.js
+++ pages/mall/index.js
@@ -1,7 +1,24 @@
-const app = getApp()
+const { statusBarHeight } = getApp().globalData
 
 Page({
   data: {
-    
+    statusBarHeight,
+    navrBarActive: false
+  },
+
+  onPageScroll(e) {
+    if (e.scrollTop >= 10) {
+      if (!this.data.navrBarActive) {
+        this.setData({
+          navrBarActive: true
+        })
+      }
+    } else {
+      if (this.data.navrBarActive) {
+        this.setData({
+          navrBarActive: false
+        })
+      }
+    }
   }
 })
