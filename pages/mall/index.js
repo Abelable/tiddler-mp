84bc7eb483b9781c356b98a544e40d93650c0178
fangkai <1209683835@qq.com>
2021-12-10 17:55:49 +0800

    feat: 商城

--- pages/mall/index.js
+++ pages/mall/index.js
@@ -3,7 +3,14 @@ const { statusBarHeight } = getApp().globalData
 Page({
   data: {
     statusBarHeight,
-    navrBarActive: false
+    categoryList: [
+      { name: '景点门票', icon: './images/ticket.png' },
+      { name: '跟团游', icon: './images/bus.png' },
+      { name: '酒店民宿', icon: './images/hotel.png' },
+      { name: '餐饮美食', icon: './images/foot.png' },
+      { name: '特色商品', icon: './images/cart.png' },
+    ],
+    navrBarActive: false,
   },
 
   onPageScroll(e) {
@@ -28,4 +35,22 @@ Page({
   onPullDownRefresh() {
     wx.stopPullDownRefresh() 
   },
+
+  search() {
+    wx.navigateTo({
+      url: './subpages/search/index'
+    })
+  },
+  navTo(e) {
+    const pageList = [
+      './subpages/ticket/index',
+      './subpages/tour/index',
+      './subpages/hotel/index',
+      './subpages/foot/index',
+      './subpages/goods/index'
+    ]
+    wx.navigateTo({
+      url: pageList[Number(e.currentTarget.dataset.index)]
+    })
+  }
 })
