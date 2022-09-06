eceb0ea45ca2fcb02e8377cea2d519c605bb1635
Administrator <admin@example.com>
2022-09-06 15:31:21 +0800

    feat: 接口请求

--- services/base/promisify.js
+++ services/base/promisify.js
@@ -1,4 +1,4 @@
-let promisify = api => {
+const promisify = api => {
   return (opt, ...arg) => {
     return new Promise((resolve, reject) => {
       api(Object.assign({}, opt, { success: resolve, fail: reject }), ...arg)
--- services/registerService.js
+++ services/registerService.js
@@ -2,11 +2,11 @@ import BaseService from './baseService'
 
 class RegisterService extends BaseService {
   async checkIsExpired() {
-    return await this.get({ url: `${this.mmsUrl}/api/v4/anchor/isExpired`, loadingTitle: '' })
+    return await this.get({ url: `${this.baseUrl}/api/v4/anchor/isExpired`, loadingTitle: '' })
   }
 
   async getMsgCenterInfo() {
-    return await this.post({ url: `${this.liveUrl}chat/message-center/msg-count`, loadingTitle: '' })
+    return await this.post({ url: `${this.baseUrl}chat/message-center/msg-count`, loadingTitle: '' })
   }
 }
 
