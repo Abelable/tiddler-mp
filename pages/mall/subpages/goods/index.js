dd138994d379e49e202bb665f52af7a9082a008e
Administrator <admin@example.com>
2022-12-21 16:02:21 +0800

    feat: 商品

--- pages/mall/subpages/goods/index.js
+++ pages/mall/subpages/goods/index.js
@@ -1,5 +1,25 @@
-Page({
-  data: {},
+const { statusBarHeight } = getApp().globalData
 
-  onLoad() {}
+Page({
+  data: {
+    statusBarHeight,
+    activeTabIdx: 0,
+    tabScroll: 0,
+  },
+
+  onLoad() {},
+
+  selectCate(e) {
+    const { idx } = e.currentTarget.dataset
+    this.setData({ 
+      activeTabIdx: idx, 
+      tabScroll: (idx - 2) * 80
+    })
+  },
+
+  navBack() {
+    wx.navigateBack({
+      delta: 1
+    })
+  }
 })
