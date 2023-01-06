f787e751c1c1fd3b7bca75e3a30de11ff7425960
Administrator <admin@example.com>
2023-01-06 11:42:51 +0800

    feat: webview

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const VERSION = 'v1'
 // 环境配置：'pro' - 正式环境, 'dev' - 开发环境
 const ENV = 'pro' 
 const API_BASE_URL = ENV === 'pro' ? 'http://localhost' : 'http://localhost'
-const WEBVIEW_BASE_URL = ENV === 'pro' ? 'http://localhost' : 'http://localhost'
+const WEBVIEW_BASE_URL = ENV === 'pro' ? 'https://h5.youboi.com/tiddler/webview/index.html#' : 'http://localhost'
 
 export {
   VERSION,
--- pages/common/webview/index.js
+++ pages/common/webview/index.js
@@ -6,34 +6,25 @@ Page({
   },
 
   async onLoad(options) {
-    let { q, url, token, ...rest } = options
-
-    if (q) {
-      const decodeQ = decodeURIComponent(q)
-      const index = decodeQ.indexOf('h5=') + 3
-      this.webviewUrl = decodeQ.substring(index)
-      checkLogin(this.initData)
-    } else {
-      for (let key in rest) {
-        if (rest.hasOwnProperty(key) && rest[key]) url += `${url.indexOf('?') === -1 ? '?' : '&'}${key}=${rest[key]}`
-      }
-      this.webviewUrl = url
-      if (token) {
-        this.setData({ 
-          url: `${this.webviewUrl}${this.webviewUrl.indexOf('?') === -1 ? '?' : '&'}token=${token}` 
-        }) 
-      } else checkLogin(this.initData)
+    let { url, ...rest } = options
+    for (let key in rest) {
+      if (rest.hasOwnProperty(key) && rest[key]) url += `${url.indexOf('?') === -1 ? '?' : '&'}${key}=${rest[key]}`
     }
+    this.webviewUrl = url
   },
 
-  initData() {
-    this.setData({
-      url: `${this.webviewUrl}${this.webviewUrl.indexOf('?') === -1 ? '?' : '&'}token=${wx.getStorageSync('token')}`
+  onShow() {
+    setTimeout(() => {
+      checkLogin(() => {
+        this.setData({
+          url: `${this.webviewUrl}${this.webviewUrl.indexOf('?') === -1 ? '?' : '&'}token=${wx.getStorageSync('token')}`
+        })
+      })
     })
   },
 
   onShareAppMessage() {
-    const path = `/pages/subpages/common/webview/index?url=${this.webviewUrl.replace('?', '&')}`
+    const path = `/pages/common/webview/index?url=${this.webviewUrl.replace('?', '&')}`
     return { path }
   }
 })
