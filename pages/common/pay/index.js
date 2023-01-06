4411e357609847b6684e8f85d8ab7c42f9c420d8
Administrator <admin@example.com>
2023-01-06 13:11:07 +0800

    feat: 商家订单支付

--- pages/common/pay/index.js
+++ pages/common/pay/index.js
@@ -1,25 +1,13 @@
 Page({
-  data: {
-  },
-
   async onLoad({ pay_params }) {
-    console.log('pay_params', decodeURIComponent(pay_params))
-    const payParams = JSON.parse(decodeURIComponent(pay_params));
+    const payParams = JSON.parse(decodeURIComponent(pay_params))
     Object.keys(payParams).forEach(
       (key) => (payParams[key] = decodeURIComponent(payParams[key]))
-    );
-    console.log('decode_pay_params', payParams)
-
+    )
     wx.requestPayment({
       ...payParams,
-      success: () => {
-        
-      },
-      fail: () => {
-        
-      },
       complete: () => {
-        
+        wx.navigateBack()
       }
     })
   },
