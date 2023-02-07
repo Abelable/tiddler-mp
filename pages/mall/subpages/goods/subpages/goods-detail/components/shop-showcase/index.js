297612e42e8ff3f6c687ccf9d3370ab2de47dae8
Administrator <admin@example.com>
2023-02-07 18:02:41 +0800

    feat: 标签修正

---
+++ pages/mall/subpages/goods/subpages/goods-detail/components/shop-showcase/index.js
@@ -0,0 +1,23 @@
+Component({
+  options: {
+    addGlobalClass: true
+  },
+  
+  properties: {
+    shopInfo: Object
+  },
+
+  methods: {
+    navToShop() {
+      const id = this.properties.shopInfo.id
+      const url = `/pages/mall/subpages/goods/subpages/shop/index?id=${id}`
+      wx.navigateTo({ url })
+    },
+
+    navToGoodsDetail(e) {
+      const id = e.currentTarget.dataset.id
+      const url = `/pages/mall/subpages/goods/subpages/goods-detail/index?id=${id}`
+      wx.navigateTo({ url })
+    }
+  }
+})
