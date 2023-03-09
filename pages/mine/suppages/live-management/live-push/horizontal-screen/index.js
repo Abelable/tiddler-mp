da52b9960e541d6b7b2d2ee33e0e15d9b4736675
Administrator <admin@example.com>
2023-03-09 10:21:33 +0800

    feat: 直播推流

--- pages/mine/suppages/live-management/live-push/horizontal-screen/index.js
+++ pages/mine/suppages/live-management/live-push/horizontal-screen/index.js
@@ -1,23 +1,47 @@
-import { store } from '../../../../../store/index'
-import { debounce } from '../../../../../utils/index'
-import LiveService from '../utils/liveService'
+import LiveService from '../../utils/liveService'
 
 const liveService = new LiveService()
 
 Page({
   data: {
-    uploadCoverLoading: false,
-    uploadShareCoverLoading: false,
-    cover: '',
-    shareCover: '',
-    direction: 1,
-    isMerchant: false,
-    pickedGoodsIds: [],
-    noticeTimeString: '',
-    isNotice: false,
+    roomInfo: null
   },
 
-  onLoad({ id }) {
-    
+  onLoad() {
+    wx.showShareMenu({
+      withShareTicket: false,
+      menus: ['shareAppMessage', 'shareTimeline']
+    })
+
+    this.setRoomInfo()
+  },
+
+  async setRoomInfo() {
+    const roomInfo = await liveService.getLivePushRoomInfo()
+    this.setData({ roomInfo })
+  },
+
+  onShow() {
+    wx.setKeepScreenOn({
+      keepScreenOn: true
+    })
+  },
+
+  onHide() {
+    wx.setKeepScreenOn({
+      keepScreenOn: false
+    })
+  },
+
+  onShareAppMessage() {
+    const { id, title, shareCover: imageUrl } = this.data.roomInfo
+    const path = `/pages/index/subpages/live/index?id=${id}`
+    return { path, title, imageUrl }
+  },
+  
+  onShareTimeline() {
+    const { id, title, shareCover: imageUrl } = this.data.roomInfo
+    const query = `id=${id}`
+    return { query, title, imageUrl }
   },
 })
