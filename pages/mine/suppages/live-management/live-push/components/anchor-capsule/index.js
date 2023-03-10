755668e192785e3f05c8b197cc3be0c3a0864963
Administrator <admin@example.com>
2023-03-10 11:18:00 +0800

    feat: 直播推流

---
+++ pages/mine/suppages/live-management/live-push/components/anchor-capsule/index.js
@@ -0,0 +1,15 @@
+import { storeBindingsBehavior } from 'mobx-miniprogram-bindings'
+import { store } from '../../../../../../../store/index'
+
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+
+  behaviors: [storeBindingsBehavior],
+
+  storeBindings: {
+    store,
+    fields: ['userInfo', 'audienceCount'],
+  },
+})
