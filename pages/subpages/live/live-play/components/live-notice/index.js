f809d7620b5315d36dd66f7da0f6d7da11c407f0
Administrator <admin@example.com>
2023-03-16 23:59:58 +0800

    feat: 首页媒体列表

--- pages/subpages/live/live-play/components/live-notice/index.js
+++ pages/subpages/live/live-play/components/live-notice/index.js
@@ -1,7 +1,7 @@
-import checkLogin from '../../../../../../../../utils/checkLogin'
-import BaseService from '../../../../../../../../service/baseService'
+import { checkLogin } from "../../../../../../utils/index";
+import LiveService from "../../../utils/liveService";
 
-const baseService = new BaseService()
+const liveService = new LiveService();
 const { statusBarHeight } = getApp().globalData.systemInfo;
 
 Component({
@@ -10,62 +10,70 @@ Component({
       type: Object,
       observer(info) {
         if (info) {
-          const { status, startTime } = info
-          !status && this.setCountDown(startTime)
+          const { status, startTime } = info;
+          !status && this.setCountDown(startTime);
         }
-      }
-    }
+      },
+    },
   },
 
   data: {
     statusBarHeight,
-    time: 0
+    time: 0,
   },
 
   detached() {
-    clearInterval(this.countDownInterval)
+    clearInterval(this.countDownInterval);
   },
 
   methods: {
     share() {
-      this.triggerEvent('share', { type: 'poster' })
+      this.triggerEvent("share", { type: "poster" });
     },
 
     toggleSubscribe() {
       checkLogin(() => {
-        const { id, previewDestine } = this.properties.roomInfo
+        const { id, previewDestine } = this.properties.roomInfo;
         if (previewDestine == 0) {
-          baseService.subscribeAnchor(id, data => {
-            if (data) {
-              wx.showToast({ title: '订阅成功', icon: 'none' })
-              this.setData({ ['roomInfo.previewDestine']: 1 })
+          liveService.subscribeAnchor(
+            id,
+            (data) => {
+              if (data) {
+                wx.showToast({ title: "订阅成功", icon: "none" });
+                this.setData({ ["roomInfo.previewDestine"]: 1 });
+              }
+            },
+            (err) => {
+              err && wx.showToast({ title: err.data.message, icon: "none" });
             }
-          }, err => {
-            err && wx.showToast({ title: err.data.message, icon: 'none' })
-          })
+          );
         } else {
-          baseService.unSubscribeAnchor(id, data => {
-            if (data) {
-              wx.showToast({ title: '取消订阅成功', icon: 'none' })
-              this.setData({ ['roomInfo.previewDestine']: 0 })
+          liveService.unSubscribeAnchor(
+            id,
+            (data) => {
+              if (data) {
+                wx.showToast({ title: "取消订阅成功", icon: "none" });
+                this.setData({ ["roomInfo.previewDestine"]: 0 });
+              }
+            },
+            (err) => {
+              err && wx.showToast({ title: err.data.message, icon: "none" });
             }
-          }, err => {
-            err && wx.showToast({ title: err.data.message, icon: 'none' })
-          })
+          );
         }
-      })
+      });
     },
 
     setCountDown(startTime) {
-      const currentTime = (new Date()).getTime() / 1000
-      let time = startTime - currentTime
-      this.setData({ time })
+      const currentTime = new Date().getTime() / 1000;
+      let time = startTime - currentTime;
+      this.setData({ time });
       this.countDownInterval = setInterval(() => {
         if (time > 0) {
-          --time
-          this.setData({ time })
-        } else clearInterval(this.countDownInterval)
-      }, 1000)
-    }
-  }
-})
+          --time;
+          this.setData({ time });
+        } else clearInterval(this.countDownInterval);
+      }, 1000);
+    },
+  },
+});
