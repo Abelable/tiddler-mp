8403c72461081093d449ff9fedd60a0506394a42
Administrator <admin@example.com>
2023-03-16 21:15:17 +0800

    feat: 首页

--- pages/tab-bar-pages/home/index.js
+++ pages/tab-bar-pages/home/index.js
@@ -1,92 +1,93 @@
 import { store } from "../../../store/index";
-const { statusBarHeight, windowHeight } = getApp().globalData.systemInfo
+const { statusBarHeight, windowHeight } = getApp().globalData.systemInfo;
 
 Component({
   data: {
     statusBarHeight,
     navBarActive: false,
-    contentWrapHeightArr: [windowHeight, windowHeight],
+    wrapHeightList: [windowHeight, windowHeight],
     curMenuIndex: 1,
   },
 
   lifetimes: {
     attached() {
-      this.scrollTopArr = [0, 0]
+      this.scrollTopArr = [0, 0];
       setTimeout(() => {
-        this.setContentWrapHeight()
-      }, 1000)
-    }
+        this.setWrapHeight();
+      }, 1000);
+    },
   },
 
   pageLifetimes: {
     show() {
       store.setTabType("home");
-    }
+    },
   },
 
   methods: {
     switchMenu(e) {
-      this.handleMenuChange(Number(e.currentTarget.dataset.index))
+      this.handleMenuChange(Number(e.currentTarget.dataset.index));
     },
-  
+
     swiperChange(e) {
-      this.handleMenuChange(Number(e.detail.current))
+      this.handleMenuChange(Number(e.detail.current));
     },
-  
+
     handleMenuChange(index) {
-      const { curMenuIndex } = this.data
+      const { curMenuIndex } = this.data;
       if (curMenuIndex !== index) {
-        this.setData({ curMenuIndex: index })
-        this.scrollTopArr[curMenuIndex] = this.scrollTop || 0
-        wx.pageScrollTo({ scrollTop: this.scrollTopArr[index] || 0, duration: 0 })
+        this.setData({ curMenuIndex: index });
+        this.scrollTopArr[curMenuIndex] = this.scrollTop || 0;
+        wx.pageScrollTo({
+          scrollTop: this.scrollTopArr[index] || 0,
+          duration: 0,
+        });
       }
     },
-  
-    setContentWrapHeight() {
-      const { curMenuIndex } = this.data
-      const query = wx.createSelectorQuery()
-      query.selectAll('.content-wrap').boundingClientRect()
-      query.exec(res => {
+
+    setWrapHeight() {
+      const { curMenuIndex } = this.data;
+      const query = wx.createSelectorQuery();
+      query.selectAll(".content-wrap").boundingClientRect();
+      query.exec((res) => {
         if (res[0][curMenuIndex]) {
-          this.setData({ 
-            [`contentWrapHeightArr[${curMenuIndex}]`]: res[0][curMenuIndex].height 
-          })
+          this.setData({
+            [`wrapHeightList[${curMenuIndex}]`]: res[0][curMenuIndex].height,
+          });
         }
-      })
+      });
     },
-  
+
     onPageScroll(e) {
       if (e.scrollTop >= 10) {
         if (!this.data.navBarActive) {
           this.setData({
-            navBarActive: true
-          })
+            navBarActive: true,
+          });
         }
       } else {
         if (this.data.navBarActive) {
           this.setData({
-            navBarActive: false
-          })
+            navBarActive: false,
+          });
         }
       }
-  
-      this.scrollTop = e.scrollTop
+
+      this.scrollTop = e.scrollTop;
     },
-  
-    onReachBottom() {
-    },
-    
+
+    onReachBottom() {},
+
     onPullDownRefresh() {
-      wx.stopPullDownRefresh() 
+      wx.stopPullDownRefresh();
     },
-  
-    signIn() {
-    },
-  
+
+    signIn() {},
+
     search() {
       wx.navigateTo({
-        url: '/pages/subpages/home/search/index'
-      })
-    }
-  }
-})
+        url: "/pages/subpages/home/search/index",
+      });
+    },
+  },
+});
