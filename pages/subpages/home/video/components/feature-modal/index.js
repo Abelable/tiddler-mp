84026db93b02d71c571dfa4968e3d3d5f4a9a5c2
Administrator <admin@example.com>
2023-03-19 21:20:51 +0800

    feat: 短视频

---
+++ pages/subpages/home/video/components/feature-modal/index.js
@@ -0,0 +1,24 @@
+import VideoService from "../../utils/videoService";
+
+const videoService = new VideoService();
+
+Component({
+  properties: {
+    videoId: Number,
+    isSticky: Boolean,
+    isPrivate: Boolean,
+  },
+
+  methods: {
+    toggleSticky() {
+      
+    },
+
+    delete() {
+      
+    },
+
+    togglePrivate() {
+    },
+  },
+});
