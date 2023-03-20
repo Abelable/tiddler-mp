d19b0368cd89d7e3cdfdb280b91bf6d89665f45f
Administrator <admin@example.com>
2023-03-20 15:39:04 +0800

    feat: 短视频评论

--- components/comment-popup/index.js
+++ components/comment-popup/index.js
@@ -1,24 +1,18 @@
-import { storeBindingsBehavior } from 'mobx-miniprogram-bindings'
-import { store } from '../../store/index'
-import BaseService from '../../services/baseService'
+import { storeBindingsBehavior } from "mobx-miniprogram-bindings";
+import { store } from "../../store/index";
+import BaseService from "../../services/baseService";
 
-const baseService = new BaseService()
+const baseService = new BaseService();
 
 Component({
   behaviors: [storeBindingsBehavior],
 
   storeBindings: {
     store,
-    actions: []
+    actions: [],
   },
 
   properties: {
-    show: {
-      type: Boolean,
-      observer(val) {
-        val ? this.setCommentsList(true) : this.initData()
-      }
-    },
     videoId: String,
     noteId: String,
   },
@@ -27,20 +21,30 @@ Component({
     totalCount: 0,
     commentsList: [],
     replyCommentId: 0,
-    replyUserName: '',
+    replyUserName: "",
     showNomore: false,
-    inputModalVisible: false
+    inputModalVisible: false,
   },
-  
+
+  lifetimes: {
+    attached() {
+      this.commentListService = this.properties.videoId
+        ? baseService.getVideoCommentList
+        : baseService.getNoteCommentList;
+      
+      this.replyCommentListService = this.properties.videoId
+      ? baseService.getVideoReplyCommentList
+      : baseService.getNoteReplyCommentList;
+    },
+  },
+
   methods: {
     initData() {
       this.setData({
         commentsList: [],
         replyCommentId: 0,
-        replyUserName: '',
         showNomore: false,
-        inputModalVisible: false
-      })
+      });
     },
 
     async setCommentsList(init = false) {
@@ -54,11 +58,11 @@ Component({
       // const { comment_count: totalCount, list: commentsList = [] } = await baseService.getCommentsLists({ videoId, commentId: initTruthy ? curCommentId : '', secCommentId: initTruthy ? curSecCommentId : '', page: ++this.page }) || {}
       // if (initTruthy) this.setData({ totalCount })
       // if (commentsList.length) {
-      //   commentsList.map(item => { 
-      //     item.replyFold = true 
+      //   commentsList.map(item => {
+      //     item.replyFold = true
       //     item.replyLists = []
       //   })
-      //   this.setData({ 
+      //   this.setData({
       //     commentsList: initTruthy ? commentsList : [...this.data.commentsList, ...commentsList]
       //   })
       //   if (initTruthy && curSecCommentId) this.toggleSpread(0)
@@ -68,66 +72,49 @@ Component({
     },
 
     spreadReply(e) {
-      const { index } = e.currentTarget.dataset
-      this.toggleSpread(index)
+      const { index } = e.currentTarget.dataset;
+      this.toggleSpread(index);
     },
 
     async toggleSpread(index) {
-      if (!this.replyPageArr[index]) this.replyPageArr[index] = 0
+      if (!this.replyPageArr[index]) this.replyPageArr[index] = 0;
 
-      const { id: parentId, replyLists, comment_num: replayCount, replyFold } = this.data.commentsList[index]
+      const {
+        id: parentId,
+        replyLists,
+        comment_num: replayCount,
+        replyFold,
+      } = this.data.commentsList[index];
       if (replayCount > replyLists.length) {
-        const { list } = await baseService.getSecondCommentsLists({ parentId, page: ++this.replyPageArr[index] })
-        if (replyFold) this.setData({ [`commentsList[${index}].replyFold`]: false })
+        const { list } = await baseService.getSecondCommentsLists({
+          parentId,
+          page: ++this.replyPageArr[index],
+        });
+        if (replyFold)
+          this.setData({ [`commentsList[${index}].replyFold`]: false });
         this.setData({
-          [`commentsList[${index}].replyLists`]: [...replyLists, ...list]
-        })
+          [`commentsList[${index}].replyLists`]: [...replyLists, ...list],
+        });
       }
       if (replayCount == replyLists.length) {
-        this.setData({ [`commentsList[${index}].replyFold`]: !replyFold })
+        this.setData({ [`commentsList[${index}].replyFold`]: !replyFold });
       }
     },
 
     atUser() {
-      this.setData({ inputModalVisible: true })
-      this.selectComponent("#inputModal").atUser()
+      this.triggerEvent("atUser");
     },
 
-    showInputModal(e) {
-      if (this.data.inputModalVisible) {
-        this.setData({ 
-          inputModalVisible: false,
-          replyCommentId: '',
-          replyUserName: ''
-        })
-      } else {
-        const { id, name } = e.detail
-        this.setData({ inputModalVisible: true })
-        if (id) {
-          this.setData({ 
-            replyCommentId: id,
-            replyUserName: name
-          })
-        }
-      }
+    comment() {
+      this.triggerEvent("comment");
     },
 
-    async sendedMsg() {
-      this.hideInputModal()
-      await this.setCommentsList(true)
-      this.setCurShortVideoCommentInfo({ id: this.properties.videoId, count: this.data.totalCount })
-    },
-
-    hideInputModal() {
-      this.data.inputModalVisible && this.setData({ 
-        inputModalVisible: false,
-        replyCommentId: 0,
-        replyUserName: ''
-      })
+    reply(e) {
+      this.triggerEvent("atUser", { commentId: e.detail.id });
     },
 
     hide() {
-      this.triggerEvent('hide')
-    }
-  }
-})
+      this.triggerEvent("hide");
+    },
+  },
+});
