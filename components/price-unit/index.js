f52226d77b5419a61b118c5324ecba0ce9f81b47
Administrator <admin@example.com>
2023-03-24 23:11:29 +0800

    feat: 直播间商品

--- components/price-unit/index.js
+++ components/price-unit/index.js
@@ -1,34 +1,38 @@
-Component({ 
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+
   properties: {
     price: {
       type: Number,
       observer(val) {
-        const arr = `${val.toFixed(2)}`.split('.')
-        const integerPart = arr[0]
-        const floatPart = arr[1] === '00' ? '' : arr[1]
-        this.setData({ integerPart, floatPart })
-      }
+        const arr = `${val.toFixed(2)}`.split(".");
+        const integerPart = arr[0];
+        const floatPart = arr[1] === "00" ? "" : arr[1];
+        this.setData({ integerPart, floatPart });
+      },
     },
     color: {
       type: String,
-      value: '#EE0D23'
+      value: "#EE0D23",
     },
     fontSize: {
       type: String,
-      value: '24rpx'
+      value: "24rpx",
     },
     integerFontSize: {
       type: String,
-      value: '32rpx'
-    }, 
+      value: "32rpx",
+    },
     symbol: {
       type: String,
-      value: '¥'
+      value: "¥",
     },
   },
 
   data: {
-    integerPart: '0',
-    floatPart: ''
+    integerPart: "0",
+    floatPart: "",
   },
-})
+});
