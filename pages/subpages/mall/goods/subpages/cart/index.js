86f4a06ddacbdb303b41e7c4b12ae8e290b31e64
Administrator <admin@example.com>
2023-04-06 21:06:25 +0800

    feat: 订单确认

--- pages/subpages/mall/goods/subpages/cart/index.js
+++ pages/subpages/mall/goods/subpages/cart/index.js
@@ -239,7 +239,7 @@ Page({
   submit(){
     if (this.data.selectedCount) {
       wx.navigateTo({ 
-        url: `//pages/subpages/mall/goods/subpages/order-check/index?cartIds=${JSON.stringify(this.selectedCartIdArr)}` 
+        url: `/pages/subpages/mall/goods/subpages/order-check/index?cartIds=${JSON.stringify(this.selectedCartIdArr)}` 
       })
     }
   },
