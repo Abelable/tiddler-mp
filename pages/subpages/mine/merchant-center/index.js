c45faca840d26d8df1b8d75035ef701556cb6c1f
Administrator <admin@example.com>
2023-08-18 21:46:30 +0800

    feat: 酒店服务商入驻

--- pages/subpages/mine/merchant-center/index.js
+++ pages/subpages/mine/merchant-center/index.js
@@ -1,5 +1,6 @@
 import { createStoreBindings } from "mobx-miniprogram-bindings";
 import { store } from "../../../../store/index";
+import { WEBVIEW_BASE_URL } from "../../../../config";
 
 Page({
   onLoad() {
@@ -11,5 +12,20 @@ Page({
 
   onUnload() {
     this.storeBindings.destroyStoreBindings();
+  },
+
+  navToScenicProviderSettleIn() {
+    const url = `/pages/subpages/common/webview/index?url=${WEBVIEW_BASE_URL}/scenic/provider/settle_in`;
+    wx.navigateTo({ url });
+  },
+
+  navToHotelProviderSettleIn() {
+    const url = `/pages/subpages/common/webview/index?url=${WEBVIEW_BASE_URL}/hotel/provider/settle_in`;
+    wx.navigateTo({ url });
+  },
+
+  navToMerchantSettleIn() {
+    const url = `/pages/subpages/common/webview/index?url=${WEBVIEW_BASE_URL}/shop/merchant/settle_in`;
+    wx.navigateTo({ url });
   },
 });
