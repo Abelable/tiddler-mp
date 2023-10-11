ef5fe948e35f592cd387291d5eede307567b589d
Administrator <admin@example.com>
2023-10-11 15:58:52 +0800

    feat: 代金券订单

---
+++ pages/subpages/mine/order-center/subpages/meal-ticket-order-list/utils/orderService.js
@@ -0,0 +1,64 @@
+import BaseService from "../../../../../../../services/baseService";
+
+class OrderService extends BaseService {
+  async getOrderList({ status, page, limit = 10 }) {
+    const { list = [] } =
+      (await this.get({
+        url: `${this.baseUrl}/catering/meal_ticket/order/list`,
+        data: { status, page, limit },
+        loadingTitle: "加载中...",
+      })) || {};
+    return list;
+  }
+
+  async getOrderDetail(id) {
+    return await this.get({
+      url: `${this.baseUrl}/catering/meal_ticket/order/detail`,
+      data: { id },
+      loadingTitle: "加载中...",
+    });
+  }
+
+  async confirmOrder(id, success) {
+    await this.post({
+      url: `${this.baseUrl}/catering/meal_ticket/order/confirm`,
+      data: { id },
+      success,
+    });
+  }
+
+  async cancelOrder(id, success) {
+    await this.post({
+      url: `${this.baseUrl}/catering/meal_ticket/order/cancel`,
+      data: { id },
+      success,
+    });
+  }
+
+  async refundOrder(id, success) {
+    await this.post({
+      url: `${this.baseUrl}/catering/meal_ticket/order/refund`,
+      data: { id },
+      success,
+    });
+  }
+
+  async deleteOrder(id, success) {
+    await this.post({
+      url: `${this.baseUrl}/catering/meal_ticket/order/delete`,
+      data: { id },
+      success,
+    });
+  }
+
+  async publishComment(order_id, comment_type, commentLists, success) {
+    return await this.post({
+      url: `${this.baseUrl}/catering/meal_ticket/order/comment`,
+      data: { order_id, comment_type, json_data: JSON.stringify(commentLists) },
+      success,
+      loadingTitle: "发布中...",
+    });
+  }
+}
+
+export default OrderService;
