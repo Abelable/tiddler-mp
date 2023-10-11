11a1409fb94771d89ad57f17da546705e67c07e0
Administrator <admin@example.com>
2023-10-11 17:05:04 +0800

    feat: 代金券订单

--- pages/subpages/mine/merchant-center/subpages/catering-management/index.js
+++ pages/subpages/mine/merchant-center/subpages/catering-management/index.js
@@ -1,106 +1,113 @@
-import { WEBVIEW_BASE_URL } from '../../../../../../config'
-import ShopService from './utils/shopService'
+import { WEBVIEW_BASE_URL } from "../../../../../../config";
+import ShopService from "./utils/shopService";
 
-const shopService = new ShopService()
-const { statusBarHeight } = getApp().globalData.systemInfo
+const shopService = new ShopService();
+const { statusBarHeight } = getApp().globalData.systemInfo;
 
 Page({
   data: {
     statusBarHeight,
     menuFixed: false,
     menuList: [
-      { name: '全部', status: 0 }, 
-      { name: '待付款', status: 1 }, 
-      { name: '待发货', status: 2 }, 
-      { name: '待收货', status: 3 }, 
-      { name: '售后', status: 5 }
+      { name: "全部", status: 0 },
+      { name: "待付款", status: 1 },
+      { name: "待发货", status: 2 },
+      { name: "待收货", status: 3 },
+      { name: "售后", status: 5 },
     ],
     curMenuIndex: 0,
     shopInfo: null,
+    orderType: 1,
     orderList: [],
     finished: false,
   },
 
   onLoad() {
-    this.setMenuTop()
+    this.setMenuTop();
   },
-  
+
   async onShow() {
     if (!this.data.shopInfo) {
-      await this.setShopInfo()
+      await this.setShopInfo();
     }
-    this.setOrderList(true)
+    this.setOrderList(true);
   },
 
   setMenuTop() {
-    const query = wx.createSelectorQuery()
-    query.select('.menu-wrap').boundingClientRect()
-    query.exec(res => {
+    const query = wx.createSelectorQuery();
+    query.select(".menu-wrap").boundingClientRect();
+    query.exec((res) => {
       if (res[0] !== null) {
-        this.menuTop = res[0].top - statusBarHeight - 44
+        this.menuTop = res[0].top - statusBarHeight - 44;
       }
-    })
+    });
   },
 
   async setShopInfo() {
-    const shopInfo = await shopService.getShopInfo()
-    this.setData({ shopInfo })
+    const shopInfo = await shopService.getShopInfo();
+    this.setData({ shopInfo });
+  },
+
+  switchOrderType() {
+    this.setData({
+      orderType: this.data.orderType === 1 ? 2 : 1,
+    });
   },
 
   selectMenu(e) {
-    const { index: curMenuIndex } = e.currentTarget.dataset
-    this.setData({ curMenuIndex })
-    this.setOrderList(true)
+    const { index: curMenuIndex } = e.currentTarget.dataset;
+    this.setData({ curMenuIndex });
+    this.setOrderList(true);
   },
 
   async setOrderList(init = false) {
-    const limit = 10
-    const { shopInfo, menuList, curMenuIndex, orderList } = this.data
-    if (init) this.page = 0
-    const list = await shopService.getOrderList({ 
-      shopId: shopInfo.id, 
-      status: menuList[curMenuIndex].status, 
-      page:  ++this.page, 
-      limit
-    })
+    const limit = 10;
+    const { shopInfo, menuList, curMenuIndex, orderList } = this.data;
+    if (init) this.page = 0;
+    const list = await shopService.getOrderList({
+      shopId: shopInfo.id,
+      status: menuList[curMenuIndex].status,
+      page: ++this.page,
+      limit,
+    });
     this.setData({
       orderList: init ? list : [...orderList, ...list],
-    })
+    });
     if (list.length < limit) {
-      this.setData({ finished: true })
+      this.setData({ finished: true });
     }
   },
 
   onPullDownRefresh() {
-    this.setOrderList(true)
-    wx.stopPullDownRefresh()
+    this.setOrderList(true);
+    wx.stopPullDownRefresh();
   },
 
   onReachBottom() {
-    this.setOrderList()
+    this.setOrderList();
   },
 
   onPageScroll(e) {
-    const { menuFixed } = this.data
+    const { menuFixed } = this.data;
     if (e.scrollTop >= this.menuTop) {
-      if (!menuFixed) this.setData({ menuFixed: true })
+      if (!menuFixed) this.setData({ menuFixed: true });
     } else {
-      if (menuFixed) this.setData({ menuFixed: false })
+      if (menuFixed) this.setData({ menuFixed: false });
     }
   },
 
   navToRestaurantManagement() {
-    const url = `/pages/subpages/common/webview/index?url=${WEBVIEW_BASE_URL}/catering/restaurant/list`
-    wx.navigateTo({ url })
+    const url = `/pages/subpages/common/webview/index?url=${WEBVIEW_BASE_URL}/catering/restaurant/list`;
+    wx.navigateTo({ url });
   },
 
   navToMealTicketManagement() {
-    const url = `/pages/subpages/common/webview/index?url=${WEBVIEW_BASE_URL}/catering/meal_ticket/list`
-    wx.navigateTo({ url })
+    const url = `/pages/subpages/common/webview/index?url=${WEBVIEW_BASE_URL}/catering/meal_ticket/list`;
+    wx.navigateTo({ url });
   },
 
   navToFreightTemplateManagement() {
-    const url = `/pages/subpages/common/webview/index?url=${WEBVIEW_BASE_URL}/shop/freight_template/list`
-    wx.navigateTo({ url })
+    const url = `/pages/subpages/common/webview/index?url=${WEBVIEW_BASE_URL}/shop/freight_template/list`;
+    wx.navigateTo({ url });
   },
-})
+});
