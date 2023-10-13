66ce65c8cf4548bf535b26a9781454e3d6231a1a
Administrator <admin@example.com>
2023-10-13 14:17:05 +0800

    feat: 餐饮套餐订单

--- pages/subpages/mine/components/set-meal-item/index.js
+++ pages/subpages/mine/components/set-meal-item/index.js
@@ -1,9 +1,46 @@
 Component({
   options: {
-    addGlobalClass: true
+    addGlobalClass: true,
   },
 
   properties: {
-    info: Object
+    info: {
+      type: Object,
+      observer({
+        price,
+        originalPrice,
+        useTimeList,
+        buyLimit,
+        perTableUsageLimit,
+        needPreBook,
+      }) {
+        const discount = parseFloat(((price / originalPrice) * 10).toFixed(1));
+
+        const tips = [];
+        if (useTimeList.length) {
+          tips.push("部分时段可用");
+        } else {
+          tips.push("营业时间可用");
+        }
+        if (buyLimit) {
+          tips.push(`限购${buyLimit}张`);
+        }
+        if (perTableUsageLimit) {
+          tips.push(`每桌限用${buyLimit}张`);
+        }
+        if (needPreBook) {
+          tips.push("需预约");
+        } else {
+          tips.push("免预约");
+        }
+
+        this.setData({ discount, tips: tips.slice(0, 3).join("｜") });
+      },
+    },
   },
-})
+
+  data: {
+    discount: 0,
+    tips: "",
+  },
+});
