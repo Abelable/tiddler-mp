9a136b75fd999dae8bf0046d62f131e29d5c4cb9
Administrator <admin@example.com>
2023-10-20 11:08:28 +0800

    feat: 实名认证

--- pages/subpages/mine/setting/index.js
+++ pages/subpages/mine/setting/index.js
@@ -1,23 +1,28 @@
-import { WEBVIEW_BASE_URL } from "../../../../config"
+import { WEBVIEW_BASE_URL } from "../../../../config";
 
 Page({
+  navToAuth() {
+    const url = `/pages/subpages/common/webview/index?url=${WEBVIEW_BASE_URL}/auth`;
+    wx.navigateTo({ url });
+  },
+
   navToScenicProviderSettleIn() {
-    const url = `/pages/subpages/common/webview/index?url=${WEBVIEW_BASE_URL}/scenic/provider/settle_in`
-    wx.navigateTo({ url })
+    const url = `/pages/subpages/common/webview/index?url=${WEBVIEW_BASE_URL}/scenic/provider/settle_in`;
+    wx.navigateTo({ url });
   },
 
   navToHotelProviderSettleIn() {
-    const url = `/pages/subpages/common/webview/index?url=${WEBVIEW_BASE_URL}/hotel/provider/settle_in`
-    wx.navigateTo({ url })
+    const url = `/pages/subpages/common/webview/index?url=${WEBVIEW_BASE_URL}/hotel/provider/settle_in`;
+    wx.navigateTo({ url });
   },
 
   navToCateringSettleIn() {
-    const url = `/pages/subpages/common/webview/index?url=${WEBVIEW_BASE_URL}/catering/provider/settle_in`
-    wx.navigateTo({ url })
+    const url = `/pages/subpages/common/webview/index?url=${WEBVIEW_BASE_URL}/catering/provider/settle_in`;
+    wx.navigateTo({ url });
   },
 
   navToMerchantSettleIn() {
-    const url = `/pages/subpages/common/webview/index?url=${WEBVIEW_BASE_URL}/shop/merchant/settle_in`
-    wx.navigateTo({ url })
+    const url = `/pages/subpages/common/webview/index?url=${WEBVIEW_BASE_URL}/shop/merchant/settle_in`;
+    wx.navigateTo({ url });
   },
-})
+});
