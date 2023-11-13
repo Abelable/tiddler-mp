b2b70b0c9592234775143e39c87d9fbf4f6a3aaa
Administrator <admin@example.com>
2023-11-13 16:46:27 +0800

    feat: 商城综合商品列表

--- pages/tab-bar-pages/mall/components/commodity-item/index.js
+++ pages/tab-bar-pages/mall/components/commodity-item/index.js
@@ -8,6 +8,11 @@ Component({
   },
 
   data: {
+    typeList: [
+      { ch: '景点', en: 'scenic' },
+      { ch: '酒店', en: 'hotel' },
+      { ch: '美食', en: 'restaurant' },
+    ],
     visible: false,
   },
 
