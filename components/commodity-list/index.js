2f22edfb6896bd135c4e70facbc69fc38c637546
Administrator <admin@example.com>
2024-04-12 22:32:24 +0800

    feat: 商品增加销量字段

---
+++ components/commodity-list/index.js
@@ -0,0 +1,40 @@
+import {
+  TYPE_OF_GOODS,
+  TYPE_OF_HOTEL,
+  TYPE_OF_RESTAURANT,
+  TYPE_OF_SCENIC
+} from "../../utils/emuns/commodityType";
+
+Component({
+  options: {
+    addGlobalClass: true
+  },
+
+  properties: {
+    list: Array
+  },
+
+  methods: {
+    checkDetail(e) {
+      const { type, id } = e.currentTarget.dataset;
+      let url;
+      switch (type) {
+        case TYPE_OF_SCENIC:
+          url = `/pages/subpages/mall/scenic-spot/subpages/spot-detail/index?id=${id}`;
+          break;
+
+        case TYPE_OF_HOTEL:
+          url = `/pages/subpages/mall/hotel/subpages/spot-detail/index?id=${id}`;
+          break;
+
+        case TYPE_OF_RESTAURANT:
+          url = `/pages/subpages/mall/catering/subpages/restaurant-detail/index?id=${id}`;
+          break;
+        case TYPE_OF_GOODS:
+          url = `/pages/subpages/mall/catering/subpages/restaurant-detail/index?id=${id}`;
+          break;
+      }
+      wx.navigateTo({ url });
+    }
+  }
+});
