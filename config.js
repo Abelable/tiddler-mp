0e80114e51e0366603848718c32dfe03ff2e9097
Administrator <admin@example.com>
2024-07-03 21:07:44 +0800

    chore: 修改配置

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const VERSION = 'v1'
 
 // 环境配置：'pro' - 正式环境, 'dev' - 开发环境
 const ENV = 'pro' 
-const API_BASE_URL = ENV === 'pro' ? 'http://localhost' : 'http://localhost'
-const WEBVIEW_BASE_URL = ENV === 'pro' ? 'https://h5.youboi.com/tiddler/webview/index.html#' : 'http://localhost'
+const API_BASE_URL = ENV === 'pro' ? 'https://api.tiddler.cn' : 'https://api.tiddler.cn'
+const WEBVIEW_BASE_URL = ENV === 'pro' ? 'https://h5.tiddler.cn' : 'https://h5.tiddler.cn'
 const QQ_MAP_KEY = 'BGCBZ-UFHWX-MBQ4O-TANN2-7WTZ3-CLBIP'
 
 export {
