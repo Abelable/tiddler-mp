cc195cf99d2541ad5b1a14b48e70ba6c1c102eb2
Administrator <admin@example.com>
2024-11-13 14:57:23 +0800

    feat: 最佳游记

--- pages/tab-bar-pages/home/components/top-media/index.js
+++ pages/tab-bar-pages/home/components/top-media/index.js
@@ -1,20 +1,51 @@
+const dayjs = require("dayjs");
+
 Component({
   options: {
-    addGlobalClass: true,
+    addGlobalClass: true
   },
-  
+
   properties: {
-    topMediaList: Array
+    topMediaList: {
+      type: Array,
+      observer(list) {
+        const mediaList = list.map(item => {
+          const { year, month: monthIdx, date} = dayjs(item.createdAt);
+          const month = [
+            "JAN",
+            "FEB",
+            "MAR",
+            "APR",
+            "MAY",
+            "JUN",
+            "JUL",
+            "AUG",
+            "SEP",
+            "OCT",
+            "NOV",
+            "DEC"
+          ][monthIdx];
+          return {
+            ...item, 
+            time: {
+              year, month, date
+            }
+          }
+        });
+        this.setData({ mediaList })
+      }
+    }
   },
 
   data: {
     curTopMediaIdx: 0,
+    mediaList: []
   },
 
   methods: {
     selectTopMedia(e) {
       const curTopMediaIdx = e.currentTarget.dataset.index;
       this.setData({ curTopMediaIdx });
-    },
+    }
   }
-})
+});
