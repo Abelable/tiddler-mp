77f6ff50c3d27447f636b3f594251234f90731b0
funguy <1209683835@qq.com>
2025-05-14 18:59:58 +0800

    feat: 家乡代言人

--- pages/tab-bar-pages/promoter/index.js
+++ pages/tab-bar-pages/promoter/index.js
@@ -1,15 +1,22 @@
 import { store } from "../../../store/index";
 
-const { statusBarHeight } = getApp().globalData.systemInfo;
-
 Component({
   data: {
-    statusBarHeight,
+    rightsList: [
+      { icon: "commission", name: "购买返利" },
+      { icon: "gift", name: "专享产品" },
+      { icon: "culture", name: "专属社群文化" },
+      { icon: "windmill", name: "文旅体验" },
+      { icon: "group", name: "代言人大会" },
+      { icon: "flag", name: "线下活动邀约" },
+      { icon: "cs", name: "1v1服务团队" },
+      { icon: "cake", name: "生日感谢礼" }
+    ],
   },
 
   pageLifetimes: {
     show() {
       store.setTabType("promoter");
     }
-  },
+  }
 });
