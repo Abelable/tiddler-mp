1f8eeb4872fbb8e7a21a9979cb3756cc5d37079c
funguy <1209683835@qq.com>
2025-05-20 11:06:07 +0800

    feat: 酒店

--- pages/subpages/mall/catering/index.js
+++ pages/subpages/mall/catering/index.js
@@ -16,18 +16,18 @@ Page({
       { icon: "", text: "综合排序", value: 0 },
       { icon: "", text: "好评排序", value: 1 },
       { icon: "", text: "价格降序", value: 2 },
-      { icon: "", text: "价格升序", value: 3 },
+      { icon: "", text: "价格升序", value: 3 }
     ],
     curCategoryId: 0,
     categoryOptions: [],
     restaurantList: [],
-    finished: false,
+    finished: false
   },
 
   async onLoad() {
     this.storeBindings = createStoreBindings(this, {
       store,
-      fields: ["checkInDate", "checkOutDate"],
+      fields: ["checkInDate", "checkOutDate"]
     });
 
     if (!store.locationInfo) {
@@ -41,7 +41,7 @@ Page({
     const options = await cateringService.getRestaurantCategoryOptions();
     const categoryOptions = [
       { icon: "", text: "全部分类", value: 0 },
-      ...options.map((item) => ({ icon: "", text: item.name, value: item.id })),
+      ...options.map(item => ({ icon: "", text: item.name, value: item.id }))
     ];
     this.setData({ categoryOptions });
   },
@@ -65,7 +65,7 @@ Page({
     if (init) {
       this.page = 0;
       this.setData({
-        finished: false,
+        finished: false
       });
     }
     const { curSortIndex, curCategoryId, restaurantList } = this.data;
@@ -89,21 +89,30 @@ Page({
         sort,
         order,
         page: ++this.page,
-        limit,
+        limit
       })) || [];
     this.setData({
-      restaurantList: init ? list : [...restaurantList, ...list],
+      restaurantList: init ? list : [...restaurantList, ...list]
     });
     if (list.length < limit) {
       this.setData({
-        finished: true,
+        finished: true
       });
     }
   },
 
+  onReachBottom() {
+    this.setRestaurantList();
+  },
+
+  onPullDownRefresh() {
+    this.setRestaurantList(true);
+    wx.stopPullDownRefresh();
+  },
+
   search() {
     wx.navigateTo({
-      url: '/pages/subpages/common/search/index?scene=6'
+      url: "/pages/subpages/common/search/index?scene=6"
     });
   },
 
@@ -113,5 +122,5 @@ Page({
 
   onUnload() {
     this.storeBindings.destroyStoreBindings();
-  },
+  }
 });
