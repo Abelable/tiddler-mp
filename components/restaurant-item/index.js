010e70dcf3ec237e7b6ff5c0cc57ef92b44334b5
funguy <1209683835@qq.com>
2025-05-22 17:42:28 +0800

    feat: 列表封面加载

--- components/restaurant-item/index.js
+++ components/restaurant-item/index.js
@@ -18,7 +18,13 @@ Component({
   },
 
   methods: {
-    onCoverLoaded() {
+    onCoverLoaded(e) {
+      const { width, height } = e.detail;
+      const coverHeight = 350 / width * height;
+      this.setData({
+        [`item.coverHeight`]:
+          coverHeight > 480 ? 480 : coverHeight < 260 ? 260 : coverHeight
+      });
       this.setData({ visible: true });
     },
 
