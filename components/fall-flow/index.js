e787bdb47884c5d09dde59ad822de074aab9b48a
funguy <1209683835@qq.com>
2025-05-28 09:06:41 +0800

    feat: 优化瀑布流

--- components/fall-flow/index.js
+++ components/fall-flow/index.js
@@ -16,6 +16,10 @@ Component({
         } else {
           this.list = newList.slice(oldList.length, newList.length);
           this.render();
+
+          // this.list = newList.slice(oldList.length, newList.length - 2);
+          // this.endList = newList.slice(-2);
+          // this.render();
         }
       }
     },
@@ -33,35 +37,120 @@ Component({
 
   methods: {
     async render() {
+      // const { leftList, rightList } = this.data;
+      // this.list.forEach((item, index) => {
+      //   if (index % 2 === 0) {
+      //     leftList.push(item);
+      //   } else {
+      //     rightList.push(item);
+      //   }
+      // });
+      // this.setData({ leftList, rightList }, async () => {
+      //   await this.completion();
+      //   this.triggerEvent("finish");
+      // });
+
+      // for (let i = 0, l = this.list.length / 2; i < l; i++) {
+      //   const { leftHeight, rightHeight } = await this.getListWrapperHeight();
+      //   if (leftHeight <= rightHeight) {
+      //     if (rightHeight - leftHeight > 200) {
+      //       this.setData({
+      //         [`leftList[${this.data.leftList.length}]`]: this.list.shift(),
+      //         [`leftList[${this.data.leftList.length}]`]: this.list.shift()
+      //       });
+      //     } else {
+      //       this.setData({
+      //         [`leftList[${this.data.leftList.length}]`]: this.list.shift(),
+      //         [`rightList[${this.data.rightList.length}]`]: this.list.shift()
+      //       });
+      //     }
+      //   } else {
+      //     if (leftHeight - rightHeight > 200) {
+      //       this.setData({
+      //         [`rightList[${this.data.rightList.length}]`]: this.list.shift(),
+      //         [`rightList[${this.data.rightList.length}]`]: this.list.shift()
+      //       });
+      //     } else {
+      //       this.setData({
+      //         [`rightList[${this.data.rightList.length}]`]: this.list.shift(),
+      //         [`leftList[${this.data.leftList.length}]`]: this.list.shift()
+      //       });
+      //     }
+      //   }
+      // }
+
       for (let i = 0, l = this.list.length / 2; i < l; i++) {
-        const { leftHeight, rightHeight } = await this.getListWrapperHeight();
-        if (leftHeight <= rightHeight) {
+        if (i === l - 1) {
+          const { leftHeight, rightHeight } = await this.getListWrapperHeight();
           if (rightHeight - leftHeight > 200) {
             this.setData({
               [`leftList[${this.data.leftList.length}]`]: this.list.shift(),
               [`leftList[${this.data.leftList.length}]`]: this.list.shift()
             });
-          } else {
+          } else if (leftHeight - rightHeight > 200) {
             this.setData({
+              [`rightList[${this.data.rightList.length}]`]: this.list.shift(),
+              [`rightList[${this.data.rightList.length}]`]: this.list.shift()
+            });
+          } else {
+             this.setData({
               [`leftList[${this.data.leftList.length}]`]: this.list.shift(),
               [`rightList[${this.data.rightList.length}]`]: this.list.shift()
             });
           }
         } else {
-          if (leftHeight - rightHeight > 200) {
-            this.setData({
-              [`rightList[${this.data.rightList.length}]`]: this.list.shift(),
-              [`rightList[${this.data.rightList.length}]`]: this.list.shift()
-            });
-          } else {
-            this.setData({
-              [`rightList[${this.data.rightList.length}]`]: this.list.shift(),
-              [`leftList[${this.data.leftList.length}]`]: this.list.shift()
-            });
-          }
+          this.setData({
+            [`leftList[${this.data.leftList.length}]`]: this.list.shift(),
+            [`rightList[${this.data.rightList.length}]`]: this.list.shift()
+          });
         }
+        // const { leftHeight, rightHeight } = await this.getListWrapperHeight();
+        // if (leftHeight <= rightHeight) {
+        //   if (rightHeight - leftHeight > 200) {
+        //     this.setData({
+        //       [`leftList[${this.data.leftList.length}]`]: this.list.shift(),
+        //       [`leftList[${this.data.leftList.length}]`]: this.list.shift()
+        //     });
+        //   } else {
+        //     this.setData({
+        //       [`leftList[${this.data.leftList.length}]`]: this.list.shift(),
+        //       [`rightList[${this.data.rightList.length}]`]: this.list.shift()
+        //     });
+        //   }
+        // } else {
+        //   if (leftHeight - rightHeight > 200) {
+        //     this.setData({
+        //       [`rightList[${this.data.rightList.length}]`]: this.list.shift(),
+        //       [`rightList[${this.data.rightList.length}]`]: this.list.shift()
+        //     });
+        //   } else {
+        //     this.setData({
+        //       [`rightList[${this.data.rightList.length}]`]: this.list.shift(),
+        //       [`leftList[${this.data.leftList.length}]`]: this.list.shift()
+        //     });
+        //   }
+        // }
       }
-      this.triggerEvent("finish");
+    },
+
+    async completion() {
+      const { leftHeight, rightHeight } = await this.getListWrapperHeight();
+      if (rightHeight - leftHeight > 200) {
+        this.setData({
+          [`leftList[${this.data.leftList.length}]`]: this.endList.shift(),
+          [`leftList[${this.data.leftList.length}]`]: this.endList.shift()
+        });
+      } else if (leftHeight - rightHeight > 200) {
+        this.setData({
+          [`rightList[${this.data.rightList.length}]`]: this.endList.shift(),
+          [`rightList[${this.data.rightList.length}]`]: this.endList.shift()
+        });
+      } else {
+        this.setData({
+          [`leftList[${this.data.leftList.length}]`]: this.endList.shift(),
+          [`rightList[${this.data.rightList.length}]`]: this.endList.shift()
+        });
+      }
     },
 
     getListWrapperHeight() {
