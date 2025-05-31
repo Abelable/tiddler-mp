c1cbb824b50447b1a166f0680049cf2059409fc6
funguy <1209683835@qq.com>
2025-05-31 14:21:24 +0800

    feat: 代言奖励

--- pages/subpages/mine/promoter/index.js
+++ pages/subpages/mine/promoter/index.js
@@ -1,4 +1,4 @@
-import { createStoreBindings } from "mobx-miniprogram-bindings";
+import { storeBindingsBehavior } from "mobx-miniprogram-bindings";
 import { store } from "../../../../store/index";
 import { WEBVIEW_BASE_URL } from "../../../../config";
 import PromoterService from "./utils/promoterService";
@@ -16,7 +16,14 @@ const rightsList = [
   { icon: "meeting", name: "代言人大会" }
 ];
 
-Page({
+Component({
+  behaviors: [storeBindingsBehavior],
+
+  storeBindings: {
+    store,
+    fields: ["userInfo", "promoterInfo"]
+  },
+
   data: {
     statusBarHeight,
     rightsList,
@@ -30,86 +37,89 @@ Page({
     customerData: null
   },
 
-  onLoad() {
-    this.storeBindings = createStoreBindings(this, {
-      store,
-      fields: ["userInfo", "promoterInfo"]
-    });
-
-    this.setAchievementInfo();
-    this.setCommissionSumInfo();
-    this.setCommissionTimeData();
-    this.setCustomerData();
-  },
-
-  async setAchievementInfo() {
-    const achievementInfo = await promoterService.getPromoterAchievement();
-    this.setData({ achievementInfo });
-  },
-
-  upgrade() {
-    const { achievementInfo, userInfo } = this.data;
-    const scene =
-      achievementInfo.percent === 100 && !userInfo.enterpriseInfoId
-        ? "certification"
-        : "performance";
-    const url = `/pages/subpages/common/webview/index?url=${WEBVIEW_BASE_URL}/promoter/${scene}`;
-    wx.navigateTo({ url });
-  },
-
-  async setCommissionSumInfo() {
-    const commissionSumInfo = await promoterService.getCommissionSumInfo();
-    this.setData({ commissionSumInfo });
-  },
-
-  selectDate(e) {
-    const curDateIdx = e.currentTarget.dataset.index;
-    this.setData({ curDateIdx });
-    this.setCommissionTimeData();
-  },
-
-  async setCommissionTimeData() {
-    const commissionTimeData = await promoterService.getCommissionTimeData(
-      this.data.curDateIdx + 1
-    );
-    this.setData({ commissionTimeData });
-  },
-
-  async setCustomerData() {
-    const customerData = await promoterService.getCustomerData();
-    this.setData({ customerData });
-  },
-
-  swiperChange(e) {
-    const curLevel = e.detail.current + 1;
-    this.setData({ curLevel });
-  },
-
-  onPageScroll(e) {
-    if (e.scrollTop >= 10) {
-      if (!this.data.navBarBgVisible) {
-        this.setData({ navBarBgVisible: true });
-      }
-    } else {
-      if (this.data.navBarBgVisible) {
-        this.setData({ navBarBgVisible: false });
+  observers: {
+    userInfo: function (userInfo) {
+      if (userInfo) {
+        this.setData({ 
+          curLevel: store.userInfo.level 
+        });
       }
     }
   },
 
-  withdraw() {
-    wx.navigateTo({
-      url: "./subpages/account/index"
-    });
-  },
+  methods: {
+    onLoad() {
+      this.setAchievementInfo();
+      this.setCommissionSumInfo();
+      this.setCommissionTimeData();
+      this.setCustomerData();
+    },
 
-  navToCustomer(e) {
-    wx.navigateTo({
-      url: `./subpages/customer/index?type=${e.currentTarget.dataset.type}`
-    });
-  },
+    async setAchievementInfo() {
+      const achievementInfo = await promoterService.getPromoterAchievement();
+      this.setData({ achievementInfo });
+    },
 
-  onUnload() {
-    this.storeBindings.destroyStoreBindings();
+    upgrade() {
+      const { achievementInfo, userInfo } = this.data;
+      const scene =
+        achievementInfo.percent === 100 && !userInfo.enterpriseInfoId
+          ? "certification"
+          : "performance";
+      const url = `/pages/subpages/common/webview/index?url=${WEBVIEW_BASE_URL}/promoter/${scene}`;
+      wx.navigateTo({ url });
+    },
+
+    async setCommissionSumInfo() {
+      const commissionSumInfo = await promoterService.getCommissionSumInfo();
+      this.setData({ commissionSumInfo });
+    },
+
+    selectDate(e) {
+      const curDateIdx = e.currentTarget.dataset.index;
+      this.setData({ curDateIdx });
+      this.setCommissionTimeData();
+    },
+
+    async setCommissionTimeData() {
+      const commissionTimeData = await promoterService.getCommissionTimeData(
+        this.data.curDateIdx + 1
+      );
+      this.setData({ commissionTimeData });
+    },
+
+    async setCustomerData() {
+      const customerData = await promoterService.getCustomerData();
+      this.setData({ customerData });
+    },
+
+    swiperChange(e) {
+      const curLevel = e.detail.current + 1;
+      this.setData({ curLevel });
+    },
+
+    onPageScroll(e) {
+      if (e.scrollTop >= 10) {
+        if (!this.data.navBarBgVisible) {
+          this.setData({ navBarBgVisible: true });
+        }
+      } else {
+        if (this.data.navBarBgVisible) {
+          this.setData({ navBarBgVisible: false });
+        }
+      }
+    },
+
+    withdraw() {
+      wx.navigateTo({
+        url: "./subpages/account/index"
+      });
+    },
+
+    navToCustomer(e) {
+      wx.navigateTo({
+        url: `./subpages/customer/index?type=${e.currentTarget.dataset.type}`
+      });
+    }
   }
 });
