1b2e0bb354a2b4a05e2e8e0f36ffa65079d08c1c
funguy <1209683835@qq.com>
2025-06-06 11:11:32 +0800

    feat: tabbar padding-bottom

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -1,17 +1,24 @@
-import { storeBindingsBehavior } from 'mobx-miniprogram-bindings'
-import { store } from '../store/index'
-      
+import { storeBindingsBehavior } from "mobx-miniprogram-bindings";
+import { store } from "../store/index";
+
+const { windowHeight, safeArea } = getApp().globalData.systemInfo;
+const safeAreaInsetBottom = Math.max(windowHeight - safeArea.bottom - 20, 10);
+
 Component({
   behaviors: [storeBindingsBehavior],
 
   storeBindings: {
     store,
-    fields: ['tabType']
+    fields: ["tabType"]
+  },
+
+  data: {
+    safeAreaInsetBottom
   },
 
   methods: {
     switchTab(e) {
-      wx.switchTab({ url: e.currentTarget.dataset.path })
+      wx.switchTab({ url: e.currentTarget.dataset.path });
     }
   }
-})
+});
