1803ba69f45ec1bf9278c9496b77651fdeb003d2
funguy <1209683835@qq.com>
2025-06-11 09:37:47 +0800

    feat: 优化缺省图按钮

--- components/empty-illus/index.js
+++ components/empty-illus/index.js
@@ -1,16 +1,9 @@
 Component({
   properties: {
     desc: String,
-    btnContent: String,
     height: {
       type: String,
       value: '800rpx'
     }
-  },
-
-  methods: {
-    btnClick() {
-      this.triggerEvent('btnClick');
-    },
   },
 });
