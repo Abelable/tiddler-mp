6514d05dfb318d73c8790a1690ff275e9d2643be
funguy <1209683835@qq.com>
2025-07-12 14:45:49 +0800

    feat: 景点门票支付

--- pages/subpages/mall/scenic/subpages/order-check/index.js
+++ pages/subpages/mall/scenic/subpages/order-check/index.js
@@ -210,12 +210,12 @@ Component({
         ...payParams,
         success: () => {
           wx.navigateTo({
-            url: "/pages/subpages/mine/order/subpages/scenic-order-list/index?status=2",
+            url: "/pages/subpages/mine/order/index?type=1&status=2",
           });
         },
         fail: () => {
           wx.navigateTo({
-            url: "/pages/subpages/mine/order/subpages/scenic-order-list/index?status=1",
+            url: "/pages/subpages/mine/order/index?type=1&status=1",
           });
         },
       });
