85b1a43b591c35cee481c0c9f78589438bcb3f8a
funguy <1209683835@qq.com>
2025-07-22 23:16:38 +0800

    feat: 商品排序

--- pages/subpages/common/webview/index.js
+++ pages/subpages/common/webview/index.js
@@ -1,4 +1,7 @@
-import { checkLogin } from "../../../../utils/index";
+import { store } from "../../../../store/index";
+import BaseService from "../../../../services/baseService";
+
+const baseService = new BaseService();
 
 Page({
   data: {
@@ -6,7 +9,18 @@ Page({
   },
 
   async onLoad(options) {
-    let { url, ...rest } = options;
+    let { url, superiorId = "", ...rest } = options;
+
+    getApp().onLaunched(async () => {
+      if (superiorId && !store.superiorInfo) {
+        wx.setStorageSync("superiorId", superiorId);
+        const superiorInfo = await baseService.getUserInfo(superiorId);
+        if (superiorInfo.promoterInfo) {
+          store.setSuperiorInfo(superiorInfo);
+        }
+      }
+    });
+
     for (let key in rest) {
       if (rest.hasOwnProperty(key) && rest[key])
         url += `${url.indexOf("?") === -1 ? "?" : "&"}${key}=${rest[key]}`;
@@ -16,21 +30,28 @@ Page({
 
   onShow() {
     setTimeout(() => {
-      checkLogin(() => {
+      const token = wx.getStorageSync("token") || "";
+      if (token) {
         this.setData({
           url: `${this.webviewUrl}${
             this.webviewUrl.indexOf("?") === -1 ? "?" : "&"
           }token=${wx.getStorageSync("token")}`
         });
-      }, false);
+      } else {
+        this.setData({
+          url: this.webviewUrl
+        });
+      }
     });
   },
 
   onShareAppMessage() {
-    const path = `/pages/subpages/common/webview/index?url=${this.webviewUrl.replace(
+    const { id } = store.promoterInfo || {};
+    const originalPath = `/pages/subpages/common/webview/index?url=${this.webviewUrl.replace(
       "?",
       "&"
     )}`;
+    const path = id ? `${originalPath}&superiorId=${id}` : originalPath;
     return { path };
   }
 });
