9f3d36a96054270a2b90317a5b47adfc7f4db6a1
funguy <1209683835@qq.com>
2025-07-26 17:32:50 +0800

    feat: 网红打卡地

--- pages/subpages/home/classification/hot-scenic/index.js
+++ pages/subpages/home/classification/hot-scenic/index.js
@@ -7,7 +7,8 @@ Page({
   data: {
     statusBarHeight,
     curMenuIdx: 0,
-    hotScenicList: []
+    hotScenicList: [],
+    scrollTop: 0
   },
 
   onLoad() {
@@ -15,8 +16,8 @@ Page({
   },
 
   selectMenu(e) {
-    const { index: curMenuIdx } = e.currentTarget.dataset;
-    this.setData({ curMenuIdx });
+    const { index } = e.currentTarget.dataset;
+    this.setData({ curMenuIdx: index, scrollTop: 296 * index });
   },
 
   async setHotScenicList() {
@@ -25,58 +26,12 @@ Page({
   },
 
   onScroll(e) {
-    if (e.detail.scrollTop < 296 && this.data.curMenuIdx !== 0) {
-      this.setData({ curMenuIdx: 0 });
-    } else if (
-      e.detail.scrollTop >= 296 &&
-      e.detail.scrollTop < 296 * 2 &&
-      this.data.curMenuIdx !== 1
-    ) {
-      this.setData({ curMenuIdx: 1 });
-    } else if (
-      e.detail.scrollTop >= 296 * 2 &&
-      e.detail.scrollTop < 296 * 3 &&
-      this.data.curMenuIdx !== 2
-    ) {
-      this.setData({ curMenuIdx: 2 });
-    } else if (
-      e.detail.scrollTop >= 296 * 3 &&
-      e.detail.scrollTop < 296 * 4 &&
-      this.data.curMenuIdx !== 3
-    ) {
-      this.setData({ curMenuIdx: 3 });
-    } else if (
-      e.detail.scrollTop >= 296 * 4 &&
-      e.detail.scrollTop < 296 * 5 &&
-      this.data.curMenuIdx !== 4
-    ) {
-      this.setData({ curMenuIdx: 4 });
-    } else if (
-      e.detail.scrollTop >= 296 * 5 &&
-      e.detail.scrollTop < 296 * 6 &&
-      this.data.curMenuIdx !== 5
-    ) {
-      this.setData({ curMenuIdx: 5 });
-    } else if (
-      e.detail.scrollTop >= 296 * 6 &&
-      e.detail.scrollTop < 296 * 7 &&
-      this.data.curMenuIdx !== 6
-    ) {
-      this.setData({ curMenuIdx: 6 });
-    } else if (
-      e.detail.scrollTop >= 296 * 7 &&
-      e.detail.scrollTop < 296 * 8 &&
-      this.data.curMenuIdx !== 7
-    ) {
-      this.setData({ curMenuIdx: 7 });
-    } else if (
-      e.detail.scrollTop >= 296 * 8 &&
-      e.detail.scrollTop < 296 * 9 &&
-      this.data.curMenuIdx !== 8
-    ) {
-      this.setData({ curMenuIdx: 8 });
-    } else if (e.detail.scrollTop >= 296 * 9 && this.data.curMenuIdx !== 9) {
-      this.setData({ curMenuIdx: 9 });
+    const scrollTop = e.detail.scrollTop;
+    const itemHeight = 296;
+    const index = Math.floor(scrollTop / itemHeight);
+
+    if (this.data.curMenuIdx !== index) {
+      this.setData({ curMenuIdx: index });
     }
   },
 
