bd17ea75b085f5fbd9c7dcc320ac79b51ae5026c
funguy <1209683835@qq.com>
2025-09-05 23:17:37 +0800

    feat: 客服聊天

--- pages/tab-bar-pages/notice/index.js
+++ pages/tab-bar-pages/notice/index.js
@@ -27,6 +27,31 @@ Component({
         e.currentTarget.dataset.info;
       const url = `/pages/subpages/notice/chat/index?userId=${friendId}&name=${friendName}&avatar=${friendAvatarUrl}`;
       wx.navigateTo({ url });
+    },
+
+    deleteConversation(e) {
+      const { id, index } = e.currentTarget.dataset;
+      const { position, instance } = e.detail;
+      if (position === "right") {
+        wx.showModal({
+          title: "提示",
+          content: "确定删除该聊天列表吗？",
+          showCancel: true,
+          success: async res => {
+            if (res.confirm) {
+              addressService.deleteAddress(id, () => {
+                const { contactList } = this.data;
+                contactList.splice(index, 1);
+                this.setData({ contactList });
+                store.tim.deleteConversation(`C2C${id}`);
+                instance.close();
+              });
+            } else {
+              instance.close();
+            }
+          }
+        });
+      }
     }
   }
 });
