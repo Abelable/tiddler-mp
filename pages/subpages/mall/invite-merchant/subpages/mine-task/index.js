86863433b9e9650ff013d3e37711c2c3492fd80c
funguy <1209683835@qq.com>
2025-10-13 23:12:13 +0800

    feat: 邀请商家

--- pages/subpages/mall/invite-merchant/subpages/mine-task/index.js
+++ pages/subpages/mall/invite-merchant/subpages/mine-task/index.js
@@ -1,7 +1,18 @@
 Page({
   data: {
+    curMenuIdx: 0
   },
 
-  onLoad() {
+  onLoad() {},
+
+  selectMenu(e) {
+    const { index: curMenuIdx } = e.currentTarget.dataset;
+    this.setData({ curMenuIdx });
   },
+
+  checkRecord() {
+    wx.navigateTo({
+      url: "../reward-decord/index"
+    });
+  }
 });
