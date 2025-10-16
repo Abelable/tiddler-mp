a2de69b8cf5f7e06263f75bc0318f33745dc69ae
funguy <1209683835@qq.com>
2025-10-16 23:25:21 +0800

    feat: 商家入驻

--- pages/subpages/mine/invite-merchant/subpages/settle-home/index.js
+++ pages/subpages/mine/invite-merchant/subpages/settle-home/index.js
@@ -3,13 +3,24 @@ const { statusBarHeight } = getApp().globalData.systemInfo;
 Page({
   data: {
     statusBarHeight,
-    headerVisible: false
+    headerVisible: false,
+    merchantTypeList: [
+      { name: "景区服务商", icon: "scenic" },
+      { name: "酒店服务商", icon: "hotel" },
+      { name: "餐饮商家", icon: "catering" },
+      { name: "电商商家", icon: "goods" }
+    ],
+    curMerchantType: 1
   },
 
-  onLoad() {
+  onLoad() {},
+
+  selectMerchantType(e) {
+    const { index } = e.currentTarget.dataset;
+    this.setData({ curMerchantType: index + 1 });
   },
 
-    onPageScroll(e) {
+  onPageScroll(e) {
     if (e.scrollTop >= 10) {
       if (!this.data.headerVisible) {
         this.setData({ headerVisible: true });
@@ -19,5 +30,5 @@ Page({
         this.setData({ headerVisible: false });
       }
     }
-  },
+  }
 });
