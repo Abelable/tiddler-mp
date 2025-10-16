1d8e69d19c52b3f495da34aca071bb63a86191a5
funguy <1209683835@qq.com>
2025-10-16 13:29:27 +0800

    feat: 任务奖励

--- pages/subpages/mine/invite-merchant/subpages/task-detail/index.js
+++ pages/subpages/mine/invite-merchant/subpages/task-detail/index.js
@@ -1,7 +1,46 @@
+import TaskService from "../../utils/taskService";
+
+const taskService = new TaskService();
+
 Page({
   data: {
+    steps: [
+      {
+        text: "步骤一",
+        desc: "描述信息",
+        inactiveIcon: "location-o",
+        activeIcon: "success"
+      },
+      {
+        text: "步骤二",
+        desc: "描述信息",
+        inactiveIcon: "like-o",
+        activeIcon: "plus"
+      },
+      {
+        text: "步骤三",
+        desc: "描述信息",
+        inactiveIcon: "star-o",
+        activeIcon: "cross"
+      },
+      {
+        text: "步骤四",
+        desc: "描述信息",
+        inactiveIcon: "phone-o",
+        activeIcon: "fail"
+      }
+    ],
+    curStep: 1,
+    qrCode: ""
   },
 
   onLoad() {
+    this.setQrcode()
   },
+
+  async setQrcode() {
+    const page = "pages/subpages/home/media/author-center/index";
+    const qrCode = await taskService.getQrCode("1", page);
+    this.setData({ qrCode });
+  }
 });
