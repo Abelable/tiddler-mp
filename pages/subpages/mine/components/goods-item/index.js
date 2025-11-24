1458e20df65cac4ad991db9ab36a6124535f840b
funguy <1209683835@qq.com>
2025-11-24 10:38:33 +0800

    chore: 商家售后

--- pages/subpages/mine/components/goods-item/index.js
+++ pages/subpages/mine/components/goods-item/index.js
@@ -4,6 +4,17 @@ Component({
   },
 
   properties: {
-    item: Object
+    item: Object,
+    navigable: Boolean
   },
-})
+
+  methods: {
+    navToGoodsDetail() {
+      const { navigable, item } = this.properties;
+      if (navigable) {
+        const url = `/pages/subpages/mall/goods/subpages/goods-detail/index?id=${item.goodsId}`;
+        wx.navigateTo({ url });
+      }
+    }
+  }
+});
